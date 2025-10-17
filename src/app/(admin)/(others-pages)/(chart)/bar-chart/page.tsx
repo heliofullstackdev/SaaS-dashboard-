144703b4dda0e17725e85840c4ec5992c4ab2c36
Helio <helio.fullstackdev@gmail.com>
2025-10-17 19:23:19 -0400

    chore: all bug fix and delete all unneccessary datas

--- src/app/(admin)/(others-pages)/(chart)/bar-chart/page.tsx
+++ src/app/(admin)/(others-pages)/(chart)/bar-chart/page.tsx
@@ -1,21 +1,14 @@
 import BarChartOne from "@/components/charts/bar/BarChartOne";
 import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import { Metadata } from "next";
 import React from "react";
-
-export const metadata: Metadata = {
-  title: "Next.js Bar Chart | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Bar Chart page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
-};
 
 export default function page() {
   return (
     <div>
-      <PageBreadcrumb pageTitle="Bar Chart" />
+      <PageBreadcrumb pageTitle="Monthly Sales" />
       <div className="space-y-6">
-        <ComponentCard title="Bar Chart 1">
+        <ComponentCard title="Monthly Sales">
           <BarChartOne />
         </ComponentCard>
       </div>
